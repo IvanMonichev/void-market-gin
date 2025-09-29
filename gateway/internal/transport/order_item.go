2b89503fc7d767f3328cd99d8b12f2119c27e387
IvanMonichev <risen15@yandex.ru>
2025-09-29 22:12:41 +0300

    feat: add created at and updated at to dto

--- gateway/internal/transport/order_item.go
+++ gateway/internal/transport/order_item.go
@@ -1,7 +1,11 @@
 package transport
 
+import "time"
+
 type OrderItemRDO struct {
-	Name      string  `json:"name"`
-	Quantity  int     `json:"quantity"`
-	UnitPrice float64 `json:"unitPrice"`
+	Name      string    `json:"name"`
+	Quantity  int       `json:"quantity"`
+	UnitPrice float64   `json:"unitPrice"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
